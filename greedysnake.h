29940b3109c90e41954cd6fa353605450675288a
willmafh <willmafh@hotmail.com>
2016-01-30 23:28:28 +0800

    Add food and snake struct to the greedysnake.h

--- greedysnake.h
+++ greedysnake.h
@@ -7,6 +7,21 @@
 #include<curses.h>
 
 #define LEV 4
+#define N 100 /* the longest of the greedy snake */
+
+struct food {
+	int row;
+	int col;
+	int flag; /* whether food should appear or not */
+};
+
+struct snake {
+	int row[N];
+	int col[N];
+	int node;
+	int direction;
+	int life;
+};
 
 void welcome(WINDOW *wel, int row, int col);
 void die(char *msg);
